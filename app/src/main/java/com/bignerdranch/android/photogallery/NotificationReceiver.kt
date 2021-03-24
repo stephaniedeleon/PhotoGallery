89bd705a5de3e03880bd4c86d53a9dc4bbb5fda9
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-03-23 21:14:37 -0700

    prevented notifications about new photos while app is running

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/NotificationReceiver.kt
@@ -0,0 +1,26 @@
+package com.bignerdranch.android.photogallery
+
+import android.app.Activity
+import android.app.Notification
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.NotificationManagerCompat
+
+private const val TAG = "NotificationReceiver"
+
+class NotificationReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.i(TAG, "received result: $resultCode")
+        if (resultCode != Activity.RESULT_OK) {
+        // A foreground activity canceled the broadcast
+            return
+        }
+        val requestCode = intent.getIntExtra(PollWorker.REQUEST_CODE, 0)
+        val notification: Notification = intent.getParcelableExtra(PollWorker.REQUEST_CODE)?:return
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(requestCode, notification)
+    }
+}
